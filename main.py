9c4478d5cc360699ec495f064c26d1e93b0860d5
Thanesh Pannirselvam <thaneshp@Thaneshs-MacBook-Air.local>
2025-04-23 21:48:53 +1000

    feat: adds initial script

---
+++ main.py
@@ -0,0 +1,51 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import OllamaEmbeddings
+from langchain_ollama.llms import OllamaLLM
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.prompts import PromptTemplate
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from sys import argv
+
+# 1. Create the model
+llm = OllamaLLM(model='gemma3:1b')
+embeddings = OllamaEmbeddings(model='nomic-embed-text')
+
+# 2. Load the PDF file and create a retriever to be used for providing context
+loader = PyPDFLoader(argv[1])
+pages = loader.load_and_split()
+store = DocArrayInMemorySearch.from_documents(pages, embedding=embeddings)
+retriever = store.as_retriever()
+
+# 3. Create the prompt template
+template = """
+Answer the question based only on the context provided.
+
+Context: {context}
+
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+def format_docs(docs):
+  return "\n\n".join(doc.page_content for doc in docs)
+
+# 4. Build the chain of operations
+chain = (
+  {
+    'context': retriever | format_docs,
+    'question': RunnablePassthrough(),
+  }
+  | prompt
+  | llm
+  | StrOutputParser()
+)
+
+# 5. Start asking questions and getting answers in a loop
+while True:
+  question = input('What do you want to learn from the document?\n')
+  print()
+  print(chain.invoke(question))
+  print()
